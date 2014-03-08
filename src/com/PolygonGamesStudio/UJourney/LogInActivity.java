c3f38775c28abfc8df9ee5e2ddfba90c3b24d9b9
sawa2012 <sawa2012@gmail.com>
2014-03-08 18:41:14 +0400

    Sign in layout - beta

---
+++ src/com/PolygonGamesStudio/UJourney/LogInActivity.java
@@ -0,0 +1,13 @@
+package com.PolygonGamesStudio.UJourney;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LogInActivity extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
