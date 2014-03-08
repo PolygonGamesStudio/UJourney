c42c9ad540aaf12b2ca13e7ca9bb5dccf1104feb
sawa2012 <sawa2012@gmail.com>
2014-03-08 19:15:45 +0400

    raw login activity

--- src/com/PolygonGamesStudio/UJourney/LogInActivity.java
+++ src/com/PolygonGamesStudio/UJourney/LogInActivity.java
@@ -3,11 +3,13 @@ package com.PolygonGamesStudio.UJourney;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class LogInActivity extends Activity{
-
+public class LogInActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.login);
     }
 }
