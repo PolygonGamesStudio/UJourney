0e5c39bee20d51222c9557166c4129c8869af83d
bruderschaft <maxim2294@gmail.com>
2014-04-19 17:47:59 +0400

    temporaly

--- src/com/PolygonGamesStudio/UJourney/ListPlacesActivity.java
+++ src/com/PolygonGamesStudio/UJourney/ListPlacesActivity.java
@@ -1,16 +1,67 @@
 package com.PolygonGamesStudio.UJourney;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.*;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import com.PolygonGamesStudio.UJourney.Adapter.MainMenuAdapter;
 
+
 public class ListPlacesActivity extends Activity{
+    private String[] leftMenuItems;
+    private CharSequence drawerTitle;
+    private CharSequence title;
+
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+
+        title = drawerTitle = getTitle();
+
+        leftMenuItems = getResources().getStringArray(R.array.menu_items);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerList = (ListView)findViewById(R.id.left_drawer);
+
+        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        drawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, leftMenuItems));
+        drawerList.setOnClickListener((View.OnClickListener) new DrawerItemClickListener());    //???
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        drawerToggle = new ActionBarDrawerToggle(
+                            this,                  /* host Activity */
+                            drawerLayout,         /* DrawerLayout object */
+                            R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+                            R.string.drawer_open,  /* "open drawer" description for accessibility */
+                            R.string.drawer_close  /* "close drawer" description for accessibility */
+                    ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(title);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(drawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        drawerLayout.setDrawerListener(drawerToggle);
 
         ListView lvMainMenu = (ListView) findViewById(R.id.listViewMainMenu);
         MainMenuAdapter adapter = new MainMenuAdapter(ListPlacesActivity.this);
@@ -18,4 +69,64 @@ public class ListPlacesActivity extends Activity{
 
 
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+       /* // Create a new fragment and specify the planet to show based on position
+        Fragment fragment = new PlanetFragment();
+        Bundle args = new Bundle();
+        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+        fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+*/
+        // Highlight the selected item, update the title, and close the drawer
+        drawerList.setItemChecked(position, true);
+        //setTitle(mPlanetTitles[position]);
+        drawerLayout.closeDrawer(drawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        /*mTitle = title;
+        getActionBar().setTitle(mTitle);*/
+    }
+
 }
