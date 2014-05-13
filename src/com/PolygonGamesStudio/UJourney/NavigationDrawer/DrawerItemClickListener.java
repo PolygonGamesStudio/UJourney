0927f215268f3ab872e13ae1c3acda2458e5754a
bruderschaft <maxim2294@gmail.com>
2014-05-13 13:25:54 +0400

    exit from navDrawer is working

---
+++ src/com/PolygonGamesStudio/UJourney/NavigationDrawer/DrawerItemClickListener.java
@@ -0,0 +1,52 @@
+package com.PolygonGamesStudio.UJourney.NavigationDrawer;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import com.PolygonGamesStudio.UJourney.LogInActivity;
+import com.PolygonGamesStudio.UJourney.ProfileActivity;
+
+/**
+ * Created by root on 4/26/14.
+ */
+public class DrawerItemClickListener implements ListView.OnItemClickListener {
+    Activity parentActivity;
+
+    public DrawerItemClickListener(Activity parentActivity) {
+        this.parentActivity = parentActivity;
+    }
+
+    @Override
+    public void onItemClick(AdapterView parent, View view, int position, long id) {
+        selectItem(position);
+    }
+
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        if (position == 3){
+            Intent intent = new Intent(parentActivity, LogInActivity.class);
+            parentActivity.startActivity(intent);
+        }
+
+        // Create a new fragment and specify the planet to show based on position
+//        Fragment fragment = new PlanetFragment();
+//        Bundle args = new Bundle();
+//        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+//        fragment.setArguments(args);
+//
+//        // Insert the fragment by replacing any existing fragment
+//        FragmentManager fragmentManager = getFragmentManager();
+//        fragmentManager.beginTransaction()
+//                .replace(R.id.content_frame, fragment)
+//                .commit();
+//
+//        // Highlight the selected item, update the title, and close the drawer
+//        mDrawerList.setItemChecked(position, true);
+//        setTitle(mPlanetTitles[position]);
+//        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+}
