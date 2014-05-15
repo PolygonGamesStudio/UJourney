31e43882b12e09806a52b652d8c781db4eee70e5
maratkanov <alexwitcher@gmail.com>
2014-05-15 18:54:56 +0400

    changed service url

--- src/com/PolygonGamesStudio/UJourney/Service/CategoryService.java
+++ src/com/PolygonGamesStudio/UJourney/Service/CategoryService.java
@@ -37,7 +37,7 @@ public class CategoryService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         // TODO: передавать URL ! Парсер json
 
-        String url = "http://192.168.1.15:5000/api/v1.0/category";
+        String url = "http://go.openrise.org:5000/api/v1.0/category";
         String jsonStr = HttpConnectionHandler.ServiceCall(url, "GET");
 
         String JSON_ROOT = "category";
