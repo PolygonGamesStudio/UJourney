85dbe40aa6659a06f39565fbfa513271b67e8a6a
bruderschaft <maxim2294@gmail.com>
2014-05-15 19:53:40 +0400

    added picasso helper

--- src/com/PolygonGamesStudio/UJourney/Helper/PicassoHelper.java
+++ src/com/PolygonGamesStudio/UJourney/Helper/PicassoHelper.java
@@ -1,35 +1,14 @@
-package com.PolygonGamesStudio.UJourney.SimpleCursorAdapter;
+package com.PolygonGamesStudio.UJourney.Helper;
 
-import android.content.Context;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.widget.ImageView;
-import android.widget.SimpleCursorAdapter;
-import com.PolygonGamesStudio.UJourney.Helper.PicassoHelper;
 import com.squareup.picasso.Transformation;
-import com.squareup.picasso.Picasso;
 
-/**
- * Created by user1 on 4/26/14.
- */
-public class JourneySimpleCursorAdapter extends SimpleCursorAdapter{
-
-    Context context_instance;
-
-    public JourneySimpleCursorAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags) {
-        super(context, layout, c, from, to, flags);
-        this.context_instance = context;
-    }
-
-    @Override
-    public void setViewImage(ImageView v, String value) {
-        int wightImage = 200;
-        int heightImage = 200;
-
-        Picasso.with(context_instance).load(value).resize(wightImage, heightImage).transform(new Transformation() {
+public class PicassoHelper {
+    public static Transformation getTransform(){
+        return new Transformation() {
             @Override
             public Bitmap transform(Bitmap source) {
                 int size = Math.min(source.getWidth(), source.getHeight());
@@ -61,6 +40,6 @@ public class JourneySimpleCursorAdapter extends SimpleCursorAdapter{
             public String key() {
                 return "circle";
             }
-        }).into(v);
+        };
     }
 }
